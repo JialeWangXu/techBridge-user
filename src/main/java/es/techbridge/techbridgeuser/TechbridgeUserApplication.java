a92d4066564998e4ebc0c56b95aee6e88928a15e
JialeWangXu <jiale.wang@alumnos.upm.es>
2026-03-28 23:54:09 +0100

    Initial TechBridge-User

---
+++ src/main/java/es/techbridge/techbridgeuser/TechbridgeUserApplication.java
@@ -0,0 +1,13 @@
+package es.techbridge.techbridgeuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechbridgeUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechbridgeUserApplication.class, args);
+    }
+
+}
---
+++ src/test/java/es/techbridge/techbridgeuser/TechbridgeUserApplicationTests.java
@@ -0,0 +1,13 @@
+package es.techbridge.techbridgeuser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechbridgeUserApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
