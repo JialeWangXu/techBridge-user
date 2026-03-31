12218370bbb9ecd2831df05d0adf041d1b0a4385
JialeWangXu <jiale.wang@alumnos.upm.es>
2026-03-31 21:30:30 +0200

    added test configuration

--- src/test/java/es/techbridge/techbridgeuser/TechbridgeUserApplicationTests.java
+++ src/test/java/es/techbridge/techbridgeuser/TechbridgeUserApplicationTests.java
@@ -2,8 +2,10 @@ package es.techbridge.techbridgeuser;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TechbridgeUserApplicationTests {
 
     @Test
